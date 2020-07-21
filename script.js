071f81adc2c837801b37dc754e0029503a40dc0e
Scott Jellen <scottjellen@scotts-MBP.fios-router.home>
2020-07-21 16:44:37 -0400

    js done needs styling

--- script.js
+++ script.js
@@ -1,50 +1,136 @@
 
 
-const calculator = document.querySelector('.calculator')
-const keys = calculator.querySelector('.calculator__keys')
-const display = document.querySelector('.calculator__display')
 
-keys.addEventListener('click', e => {
-    if (e.target.matches('button')) {
-    const key = e.target
-    const action = key.dataset.action
-    const keyContent = key.textContent
-    const displayedNum = display.textContent
-    const previousKeyType = calculator.dataset.previousKeyType
 
-    if (!action) {
-        if (displayedNum === '0' || previousKeyType === 'operator') {
-          display.textContent = keyContent
-        } else {
-          display.textContent = displayedNum + keyContent
-        }
-      }
-
-    if (action === 'decimal') {
-        display.textContent = displayedNum + '.'
-      }
-    if (
-        action === 'add' ||
-        action === 'subtract' ||
-        action === 'multiply' ||
-        action === 'divide'
-    ) {
-        key.classList.add('is-depressed')
-
-        calculator.dataset.previousKeyType = 'operator'
-        
-    }
-
-   
-
-    Array.from(key.parentNode.children)
-      .forEach(k => k.classList.remove('is-depressed'))
+const calculator = {
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null
 }
+
+function resetCalculator() {
+  calculator.displayValue = '0',
+  calculator.firstOperand = null,
+  calculator.waitingForSecondOperand = false,
+  calculator.operator = null
+}
+
+
+
+function inputDigit(digit) {
+  const { displayValue, waitingForSecondOperand } = calculator
+
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = digit
+    calculator.waitingForSecondOperand = false
+  } else {
+    calculator.displayValue = displayValue === '0'? digit : displayValue + digit
+    
+  }
+
+  
+}
+
+function inputDecimal(dot) {
+  if (calculator.waitingForSecondOperand === true) {
+    calculator.displayValue = '0.'
+    calculator.waitingForSecondOperand = false
+    return
+  }
+
+
+  if (!calculator.displayValue.includes(dot)) {
+    calculator.displayValue += dot
+  }
+}
+
+function updateDisplay() {
+  const display = document.querySelector('.calculator-screen')
+  display.value = calculator.displayValue 
+}
+
+
+function handleOperator(nextOperator) {
+  const { firstOperand, displayValue, operator } = calculator
+
+  const inputValue = parseFloat(displayValue)
+
+  if (operator && calculator.waitingForSecondOperand) {
+    calculator.operator = nextOperator
+    
+    return
+  }
+
+  if (firstOperand === null && !isNaN(inputValue)) {
+    calculator.firstOperand = inputValue
+  } else if (operator) {
+    const result = calculate(firstOperand, inputValue, operator)
+
+    calculator.displayValue = `${parseFloat(result.toFixed(7))}`
+    calculator.firstOperand = result
+  }
+
+
+  calculator.waitingForSecondOperand = true
+  calculator.operator = nextOperator
+
+  
+}
+
+function calculate(firstOperand, secondOperand, operator) {
+  if (operator === "+") {
+    return firstOperand + secondOperand
+  } else if (operator === "-") {
+    return firstOperand - secondOperand
+  } else if (operator === "*") {
+    return firstOperand * secondOperand
+  } else if (operator === "/") {
+    return firstOperand / secondOperand
+  }
+
+  return secondOperand
+
+}
+
+updateDisplay()
+
+
+
+const keys = document.querySelector('.calculator-keys')
+keys.addEventListener('click', event => {
+  const { target } = event
+  const { value } = target
+
+  if (!target.matches('button')) {
+    return
+  }
+
+  switch (value) {
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '=':
+      handleOperator(value)
+      break
+    case '.':
+      inputDecimal(value)
+      break
+    case 'all-clear':
+      resetCalculator()
+      break
+    default:
+      if (Number.isInteger(parseFloat(value)))  {
+        inputDigit(value)
+      }
+
+    
+
+
+
+  }
+    updateDisplay()
+
+
 })
-
-
-
-
-
-
-
